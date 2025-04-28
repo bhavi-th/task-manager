925fc957cfcfc68b0487a06a75e3dc97f1acce62
Bhavith <bhaviths15@gmail.com>
2025-04-28 21:30:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './App.css';
-import Calendar from "./Calendar.js";
 
 function App() {
   return (
-    <div className="App">
-      <Calendar />
-    </div>
+    <div className="App"></div>
   );
 }
 
