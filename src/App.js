83eb0d7d7c669ac0f765f509aa502caff04b7501
Bhavith <bhaviths15@gmail.com>
2025-05-01 23:13:39 +0530

    Added input and add button.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <fieldset>
+        <legend>Task Manager</legend>
+      </fieldset>
+      <fieldset id="tasks">
+        <legend>TO DO's</legend>
+        <div id="task-input">
+          <input placeholder="Enter your task" />
+          <button>Add</button>
+        </div>
+      </fieldset>
+    </div>
   );
 }
 
