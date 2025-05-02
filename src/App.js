49cfb7b05169bba4754e5398ae0d2130fc9b54b5
Bhavith <bhaviths15@gmail.com>
2025-05-02 13:56:54 +0530

    Enable adding tasks by hitting Enter in input field

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import { useRef } from "react";
 
 function App() {
+
+  const input = useRef();
+  const list = useRef();
+
+  function handleSubmit() {
+    if (input.current.value !== "") {
+      const li = document.createElement('li');
+      li.textContent = input.current.value;
+      list.current.appendChild(li);
+    }
+  }
+
   return (
     <div className="App">
       <fieldset>
@@ -8,10 +21,12 @@ function App() {
       </fieldset>
       <fieldset id="tasks">
         <legend>TO DO's</legend>
-        <div id="task-input">
-          <input placeholder="Enter your task" />
-          <button>Add</button>
-        </div>
+        <form id="task-input" onSubmit={handleSubmit} action="#">
+          <input placeholder="Enter your task" ref={input} />
+        </form>
+        <ol ref={list}>
+          <li>First Item</li>
+        </ol>
       </fieldset>
     </div>
   );
